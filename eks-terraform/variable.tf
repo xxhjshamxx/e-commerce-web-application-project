b2cb74fbd251ff57054553036e534eb41e51d725
xxhjshamxx <eltaweelh3@gmail.com>
2026-07-09 17:42:53 +0300

    setup local_env

--- eks-terraform/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "node_group_name" {
-  description = "Name of the EKS node group"
-  type        = string
-  default     = "project-node-group"
-}
--- s3-buckets/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket1_id" {
-  description = "ID of the first S3 bucket"
-  value       = aws_s3_bucket.bucket1.id
-}
-
-output "bucket2_id" {
-  description = "ID of the second S3 bucket"
-  value       = aws_s3_bucket.bucket2.id
-}
